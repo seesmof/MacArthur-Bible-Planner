a3894212e301fa8785c0d42d0ce5a306e2a7df42
Oleg Onyshchenko <seesmwork@gmail.com>
2026-07-03 10:57:01 +0300

    feat: add the nextjs app

---
+++ client/app/page.tsx
@@ -0,0 +1,8 @@
+export default function IndexPage() {
+  return (
+    <div className="p-3">
+      <div>1 John</div>
+      <div>John 1-7</div>
+    </div>
+  );
+}
---
+++ plan.md
@@ -0,0 +1,1 @@
+How to make a table with 30 columns and N rows?
