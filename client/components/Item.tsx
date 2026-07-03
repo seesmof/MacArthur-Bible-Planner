a210121f5367c492a7a29495fcb5b95d62381811
Oleg Onyshchenko <seesmwork@gmail.com>
2026-07-03 11:32:35 +0300

    feat: add an item

--- client/components/Item.tsx
+++ client/components/Item.tsx
@@ -1,7 +1,31 @@
+"use client";
+
+import { useState } from "react";
+
 interface ItemProps {
   name: string;
 }
 
 export default function Item({ name }: ItemProps) {
-  return <div className="">{name}</div>;
+  const [count, setCount] = useState<number>(0);
+  const [isCompleted, setIsCompleted] = useState<boolean>(false);
+
+  return (
+    <div className="flex gap-3 items-center">
+      <button
+        className="btn"
+        disabled={isCompleted}
+        onClick={() => {
+          if (count === 29) setIsCompleted(true);
+          if (!isCompleted) setCount((count) => count + 1);
+        }}
+      >
+        {name}
+      </button>
+      <p>read {count} times.</p>
+      <button className="btn" onClick={() => setCount(0)}>
+        Reset
+      </button>
+    </div>
+  );
 }
